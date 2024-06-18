07123383e4e258340103dcd43b673554b761706e
k3na1 <franaranguizi@gmail.com>
2024-06-18 11:05:56 -0400

    Código de la calculadora promedio, mediana y moda

---
+++ codigo.py
@@ -0,0 +1,15 @@
+import statistics as st
+
+
+
+try:
+    with open('datos.txt', 'r') as datos:
+        numeros = datos.read().splitlines()
+        intConvertor = [int (i) for i in numeros]                   # Convertidor de string a integer
+        numeros = intConvertor
+
+        mediana = st.median(numeros)
+        print(mediana)
+
+except:
+    print("ERROR: ERROR EN EL PROGRAMA. CONSULTAR AL DUEÑO")
